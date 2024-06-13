069f18fcc2392e6300ff88063a9f472eb7916d8f
unknown <rutilohahn@hotmail.com>
2024-06-13 14:05:52 -0300

    Projeto atualizado com API de tradução

---
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch.service;
+
+import com.theokanning.openai.OpenAiHttpException;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChatGPT {
+    public static String obterTraducao(String texto) {
+        try {
+            OpenAiService service = new OpenAiService("sk-proj-Rf91z8NttkF7yfK1SHAjT3BlbkFJxAGRo8a5J50ywwmXSIeM");
+
+            CompletionRequest requisicao = CompletionRequest.builder()
+                    .model("gpt-3.5-turbo-instruct")
+                    .prompt("traduza para o português o texto: " + texto)
+                    .maxTokens(1000)
+                    .temperature(0.7)
+                    .build();
+
+            var resposta = service.createCompletion(requisicao);
+            return resposta.getChoices().get(0).getText();
+        } catch (OpenAiHttpException e) {
+            String error;
+            if (e.getMessage().contains("You exceeded your current quota")) {
+               error = "Desculpe, excedemos a cota de uso da API. Por favor, tente novamente mais tarde.";
+            } else {
+                throw e; // Relança a exceção se não for o erro de cota
+            }
+            return error;
+        }
+    }
+}
+
