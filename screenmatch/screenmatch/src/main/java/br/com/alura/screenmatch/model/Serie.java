58ef37070a006e299ef6bb89ae97f4bedd824766
unknown <rutilohahn@hotmail.com>
2024-06-14 17:40:12 -0300

    Realizada a conexão com o banco de dados PostgreSQL

--- screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,19 +1,31 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
 import static br.com.alura.screenmatch.service.ConsultaChatGPT.obterTraducao;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String poster;
     private String sinopse;
+    @Transient
+    private List<Episodio> episodios = new ArrayList<>();
 
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
@@ -22,12 +34,13 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+        this.sinopse = dadosSerie.sinopse();
+        /*this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
         if (obterTraducao(dadosSerie.sinopse()).trim().equals("Desculpe, excedemos a cota de uso da API. Por favor, tente novamente mais tarde.")){
             this.sinopse = dadosSerie.sinopse();
         } else {
             this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
-        }
+        }*/
     }
 
     public String getTitulo() {
@@ -84,6 +97,22 @@ public class Serie {
 
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        this.episodios = episodios;
     }
 
     @Override
